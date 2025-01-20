611edf1991a8c166bddd792884b60611de16f393
rsp2308 <roshan.parmar2024@lpu.in>
2025-01-20 23:57:04 +0530

    vite_deps

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ const App = () => {
   return (
     <div>
       <form className='bg-yellow-300 flex flex-col items-center space-y-4'>
-        <input type="text" />
-      <button className='bg-green-700'>DO It NOw</button>
+        <input type="text" placeholder='pls enter here' />
+      <button className='bg-green-700 p-2 rounded'>BANGER</button>
       </form>
     </div>
   )
